3ec0edb6a7c4a2cd5597653afc111ca9c7a60bf1
Oscar Branson <ob266@cam.ac.uk>
2024-11-04 21:40:46 +0000

    get_ABC_period function

--- monitor/co2.py
+++ monitor/co2.py
@@ -6,8 +6,9 @@ from pathlib import Path
 
 commands = {
     'read': b'\xfe\x04\x00\x03\x00\x01\xd5\xc5',
-    'disable_ABC': b'\xfe\x06\x00\x1f\x00\x00\xac\x03',
-    'enable_ABC_start': b'\xfe\x06\x00\x1f'
+    'ABC_disable': b'\xfe\x06\x00\x1f\x00\x00\xac\x03',
+    'ABC_enable_start': b'\xfe\x06\x00\x1f',
+    'ABC_get_period': b'\xfe\x03\x00\x1f\x00\x01\xa1\xc3'
 }
 
 def calculate_crc16(data: bytes) -> bytes:
@@ -30,7 +31,8 @@ class SensorError(Exception):
 class Sensor:
     def __init__(self, port='/dev/ttyS0'):
         self.port = port
-        self.comm = None
+        
+        self.connect()
             
     def __enter__(self):
         self.open()
@@ -40,15 +42,16 @@ class Sensor:
         self.close()
         return False
     
+    def connect(self):
+        self.comm = serial.Serial(self.port, 9600, timeout=1, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+    
     def open(self):
-        if not self.comm:
-            self.comm = serial.Serial(self.port, 9600, timeout=1, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-            self.ABC_on = False
+        if not self.comm.isOpen():
+            self.comm.open()
                 
     def close(self):
         if self.comm and self.comm.is_open:
             self.comm.close()
-            self.comm = None
     
     def read(self):
         self.comm.flushInput()
@@ -57,7 +60,7 @@ class Sensor:
         sleep(0.05)
         
         if self.comm.in_waiting > 0:
-            data = self.comm.read(7)
+            data = self.comm.read(self.comm.in_waiting)
             sleep(0.05)
             value = int.from_bytes(data[3:5], byteorder='big')
         else:
@@ -73,9 +76,10 @@ class Sensor:
         self.comm.write(commands['disable_ABC'])
         sleep(0.05)
         
-        data = self.comm.read(8)
+        if self.comm.in_waiting > 0:
+            data = self.comm.read(self.comm.in_waiting)
         
-        self.ABC_on = not (data == commands['disable_ABC'])
+            self.ABC_on = not (data == commands['disable_ABC'])
         
         self.comm.flushOutput()
 
@@ -91,12 +95,29 @@ class Sensor:
         self.comm.write(msg)
         sleep(0.05)
         
-        data = self.comm.read(8)
+        if self.comm.in_waiting > 0:
+            data = self.comm.read(self.comm.in_waiting)
         
-        self.ABC_on = data == msg
+            self.ABC_on = data == msg
         
         self.comm.flushOutput()
 
+    def get_ABC_period(self):
+        self.comm.flushInput()
+        
+        self.comm.write(commands['get_ABC_period'])
+        sleep(0.05)
+        
+        if self.comm.in_waiting > 0:
+            data = self.comm.read(self.comm.in_waiting)
+            period = int.from_bytes(data[3:5], byteorder='big')
+        else:
+            raise SensorError('No response from CO2 sensor. Is it connected?')
+        
+        self.comm.flushOutput()
+        
+        return period
+    
     
     def close(self):
         self.comm.close()
